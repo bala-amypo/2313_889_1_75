201f634428111fcf383cda2c9a8d68413b1cc981
bala-amypo <bala@amypo.in>
2025-12-18 18:58:19 +0000

    User commit

--- src/main/java/com/example/demo/controller/VisitorController.java
+++ src/main/java/com/example/demo/controller/VisitorController.java
@@ -1,14 +1,7 @@
 package com.example.demo.controller;
 
 import java.util.List;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 import com.example.demo.model.Visitor;
 import com.example.demo.service.VisitorService;
 
@@ -18,22 +11,14 @@ public class VisitorController {
 
     private final VisitorService src;
 
-    public VisitorController(VisitorService src) {
-        this.src = src;
-    }
+    public VisitorController(VisitorService src) { this.src = src; }
 
     @PostMapping("/post")
-    public Visitor insertData(@RequestBody Visitor st) {
-        return src.postdata(st);
-    }
+    public Visitor insertdata(@RequestBody Visitor st) { return src.postdata(st); }
 
     @GetMapping("/get")
-    public List<Visitor> wantData() {
-        return src.getdata();
-    }
+    public List<Visitor> wantData() { return src.getdata(); }
 
     @GetMapping("/getid/{id}")
-    public Visitor particularData(@PathVariable Long id) {
-        return src.getidvalue(id);
-    }
+    public Visitor particulardata(@PathVariable Long id) { return src.getidvalue(id); }
 }

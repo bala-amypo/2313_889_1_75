56da0e5e8219160d45510ae7ad4a3faa7111ae0c
bala-amypo <bala@amypo.in>
2025-12-18 08:43:11 +0000

    User commit

--- src/main/java/com/example/demo/model/VisitorEntity.java
+++ src/main/java/com/example/demo/model/VisitorEntity.java
@@ -1,6 +1,65 @@
-pakage com.example.demo.model;
+package com.example.demo.model;
 
+import java.time.LocalDateTime;
 
-public class VisitorEntity{
-    
+import jakarta.persistence.*;
+
+public class VisitorEntity {
+@Id
+    private Long id;
+    private String fullname;
+    @column(unique=true)
+    private String email; 
+    private String phone;
+    private String idproof;
+    private LocalDateTime createdAt;
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getFullname() {
+        return fullname;
+    }
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPhone() {
+        return phone;
+    }
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+    public String getIdproof() {
+        return idproof;
+    }
+    public void setIdproof(String idproof) {
+        this.idproof = idproof;
+    }
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+    public VisitorEntity() {
+    }
+    public VisitorEntity(Long id, String fullname, String email, String phone, String idproof,
+            LocalDateTime createdAt) {
+        this.id = id;
+        this.fullname = fullname;
+        this.email = email;
+        this.phone = phone;
+        this.idproof = idproof;
+        this.createdAt = createdAt;
+    }
+ 
+
 }

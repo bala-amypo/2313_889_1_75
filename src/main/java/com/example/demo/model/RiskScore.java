b01c0ef8b22dc8f3fc958c834369f74e320b35fa
bala-amypo <bala@amypo.in>
2025-12-18 14:54:03 +0000

    User commit

--- src/main/java/com/example/demo/model/RiskScore.java
+++ src/main/java/com/example/demo/model/RiskScore.java
@@ -1,67 +1,72 @@
-pakage com.example.demo.model;
+package com.example.demo.model;
+import java.time.LocalDate;
+
 import jakarta.persistence.*;
-import java.time.LocalDateTime;
-
-@Entity
-@Table(name = "risk_scores")
 public class RiskScore {
-
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+     private Long id;
 
-    @OneToOne(optional = false)
-    @JoinColumn(name = "visitor_id")
-    private Visitor visitor;
-
-    @Column(nullable = false)
-    private Integer totalScore;
-
-    @Column(nullable = false)
-    private String riskLevel; 
-    // LOW / MEDIUM / HIGH / CRITICAL
-
-    @Column(nullable = false)
-    private LocalDateTime evaluatedAt;
-
-    @PrePersist
-    @PreUpdate
-    public void evaluateRisk() {
-        if (totalScore == null || totalScore < 0) {
-            throw new RuntimeException("totalScore must be >= 0");
-        }
-
-        this.riskLevel = RiskLevelUtils.getRiskLevel(totalScore);
-        this.evaluatedAt = LocalDateTime.now();
-    }
-
-    // Getters and Setters
-
+     @Min(0)
+     private int totalScore;
+    private String LOW;
+    private String MEDIUM;
+    private String HIGH;
+    private String CRITICAL;
+    private LocalDate evaluatedAt;
     public Long getId() {
         return id;
     }
-
-    public Visitor getVisitor() {
-        return visitor;
+    public void setId(Long id) {
+        this.id = id;
     }
-
-    public void setVisitor(Visitor visitor) {
-        this.visitor = visitor;
-    }
-
-    public Integer getTotalScore() {
+    public int getTotalScore() {
         return totalScore;
     }
-
-    public void setTotalScore(Integer totalScore) {
+    public void setTotalScore(int totalScore) {
         this.totalScore = totalScore;
     }
-
-    public String getRiskLevel() {
-        return riskLevel;
+    public String getLOW() {
+        return LOW;
     }
-
-    public LocalDateTime getEvaluatedAt() {
+    public void setLOW(String lOW) {
+        LOW = lOW;
+    }
+    public String getMEDIUM() {
+        return MEDIUM;
+    }
+    public void setMEDIUM(String mEDIUM) {
+        MEDIUM = mEDIUM;
+    }
+    public String getHIGH() {
+        return HIGH;
+    }
+    public void setHIGH(String hIGH) {
+        HIGH = hIGH;
+    }
+    public String getCRITICAL() {
+        return CRITICAL;
+    }
+    public void setCRITICAL(String cRITICAL) {
+        CRITICAL = cRITICAL;
+    }
+    public LocalDate getEvaluatedAt() {
         return evaluatedAt;
     }
+    public void setEvaluatedAt(LocalDate evaluatedAt) {
+        this.evaluatedAt = evaluatedAt;
+    }
+    public RiskScore(Long id, int totalScore, String lOW, String mEDIUM, String hIGH, String cRITICAL,
+            LocalDate evaluatedAt) {
+        this.id = id;
+        this.totalScore = totalScore;
+        LOW = lOW;
+        MEDIUM = mEDIUM;
+        HIGH = hIGH;
+        CRITICAL = cRITICAL;
+        this.evaluatedAt = evaluatedAt;
+    }
+    public RiskScore() {
+    }
+    
+    
 }
--- src/main/java/com/example/demo/model/ScoreAuditLog.java
+++ src/main/java/com/example/demo/model/ScoreAuditLog.java
@@ -1,84 +1,40 @@
-pakage com.example.demo.model;
+package com.example.demo.model;
+import java.time.LocalDate;
+
 import jakarta.persistence.*;
-import java.time.LocalDateTime;
-
-@Entity
-@Table(name = "score_audit_logs")
+import jakarta.xml.bind.annotation.XmlID;
 public class ScoreAuditLog {
-
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @ManyToOne(optional = false)
-    @JoinColumn(name = "visitor_id")
-    private Visitor visitor;
-
-    @ManyToOne(optional = false)
-    @JoinColumn(name = "risk_rule_id")
-    private RiskRule appliedRule;
-
-    @Column(nullable = false)
-    private Integer scoreChange;
-
-    @Column(nullable = false)
+    private Long private;
+    @Min(0)
+    private int scoreChange;
     private String reason;
-
-    @Column(nullable = false)
-    private LocalDateTime loggedAt;
-
-    @PrePersist
-    public void onLog() {
-        if (scoreChange == null || scoreChange < 0) {
-            throw new RuntimeException("scoreChange must be >= 0");
-        }
-
-        if (reason == null || reason.trim().isEmpty()) {
-            throw new RuntimeException("reason required");
-        }
-
-        this.loggedAt = LocalDateTime.now();
-    }
-
-    // Getters and Setters
-
-    public Long getId() {
-        return id;
-    }
-
-    public Visitor getVisitor() {
-        return visitor;
-    }
-
-    public void setVisitor(Visitor visitor) {
-        this.visitor = visitor;
-    }
-
-    public RiskRule getAppliedRule() {
-        return appliedRule;
-    }
-
-    public void setAppliedRule(RiskRule appliedRule) {
-        this.appliedRule = appliedRule;
-    }
-
-    public Integer getScoreChange() {
+    private LocalDate loggedAt;
+    public int getScoreChange() {
         return scoreChange;
     }
-
-    public void setScoreChange(Integer scoreChange) {
+    public void setScoreChange(int scoreChange) {
         this.scoreChange = scoreChange;
     }
-
     public String getReason() {
         return reason;
     }
-
     public void setReason(String reason) {
         this.reason = reason;
     }
-
-    public LocalDateTime getLoggedAt() {
+    public LocalDate getLoggedAt() {
         return loggedAt;
     }
+    public void setLoggedAt(LocalDate loggedAt) {
+        this.loggedAt = loggedAt;
+    }
+    public ScoreAuditLog(int scoreChange, String reason, LocalDate loggedAt) {
+        this.scoreChange = scoreChange;
+        this.reason = reason;
+        this.loggedAt = loggedAt;
+    }
+    public ScoreAuditLog() {
+    }
+
+
 }

c03c7171c60a9308c316f167360da38064510aa0
bala-amypo <bala@amypo.in>
2025-12-22 07:55:00 +0000

    User commit

--- src/main/java/com/example/demo/model/RiskRule.java
+++ src/main/java/com/example/demo/model/RiskRule.java
@@ -44,7 +44,7 @@ public class RiskRule {
         this.createdAt = createdAt;
     }
 
-    // Getters and Setters
+    
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
     public String getRuleName() { return ruleName; }

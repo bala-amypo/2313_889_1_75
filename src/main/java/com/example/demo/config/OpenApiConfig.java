baf83d39dd2c6c6f3a9f0bd4e4770ea49c4661d3
bala-amypo <bala@amypo.in>
2025-12-29 02:02:58 +0000

    User commit

--- src/main/java/com/example/demo/config/OpenApiConfig.java
+++ src/main/java/com/example/demo/config/OpenApiConfig.java
@@ -2,8 +2,12 @@ package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.servers.Server;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.Components;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import java.util.List;
 
 @Configuration
@@ -11,10 +15,22 @@ public class OpenApiConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
+
+        // Define Basic Authentication scheme
+        SecurityScheme basicAuthScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("basic");
+
         return new OpenAPI()
-                // You need to change the port as per your server
+                .components(
+                        new Components()
+                                .addSecuritySchemes("basicAuth", basicAuthScheme)
+                )
+                .addSecurityItem(
+                        new SecurityRequirement().addList("basicAuth")
+                )
                 .servers(List.of(
                         new Server().url("https://9269.pro604cr.amypo.ai/")
                 ));
-        }
+    }
 }
